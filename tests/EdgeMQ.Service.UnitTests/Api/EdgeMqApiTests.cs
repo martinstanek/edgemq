1fdfaae3bcdf61ab03434aa0838d2bae09ca430d
Martin Stanek <stanek@awitec.cz>
2024-12-27 12:50:10 +0100

    tests

--- tests/EdgeMQ.Service.UnitTests/Api/EdgeMqApiTests.cs
+++ tests/EdgeMQ.Service.UnitTests/Api/EdgeMqApiTests.cs
@@ -12,32 +12,57 @@ public sealed class EdgeMqApiTests
     [Fact]
     public async Task Peek_NoMessagesAdded_QueueIsEmpty()
     {
+        const string queueName = "test-queue";
+
         var context = new EdgeMqApiTestsContext();
         var client = context.GetClient();
 
-        var messages = await client.PeekAsync(queueName: "test-queue", batchSize: 100);
-        var stats = await client.GetMetricsAsync(queueName: "test-queue");
+        var messages = await client.PeekAsync(queueName, batchSize: 100);
+        var stats = await client.GetMetricsAsync(queueName);
 
         messages.ShouldBeEmpty();
-        stats.Name.ShouldBe("test-queue");
+        stats.Name.ShouldBe(queueName);
         stats.MessageCount.ShouldBe((uint)0);
     }
 
     [Fact]
     public async Task Dequeue_MessagesAdded_QueueIsEmpty()
     {
+        const string queueName = "test-queue";
         const string payload = "hallo";
 
         var context = new EdgeMqApiTestsContext();
         var client = context.GetClient();
 
-        await client.EnqueueAsync("test-queue", payload);
-        var messages = await client.DequeueAsync(queueName: "test-queue", batchSize: 100);
-        var stats = await client.GetMetricsAsync(queueName: "test-queue");
+        await client.EnqueueAsync(queueName, payload);
+        var messages = await client.DequeueAsync(queueName, batchSize: 100);
+        var stats = await client.GetMetricsAsync(queueName);
 
         messages.Count.ShouldBe(1);
         messages.First().Payload.ShouldBe(payload);
-        stats.Name.ShouldBe("test-queue");
+        stats.Name.ShouldBe(queueName);
+        stats.MessageCount.ShouldBe((uint)0);
+    }
+
+    [Fact]
+    public async Task Acknowledge_MessagesAdded_QueueIsEmpty()
+    {
+        const string queueName = "test-queue";
+        const string payload = "hallo";
+
+        var context = new EdgeMqApiTestsContext();
+        var client = context.GetClient();
+
+        await client.EnqueueAsync(queueName, payload);
+        await client.EnqueueAsync(queueName, payload);
+
+        var messages = await client.PeekAsync(queueName, batchSize: 100);
+
+        await client.AcknowledgeAsync(queueName, messages.First().BatchId);
+
+        var stats = await client.GetMetricsAsync(queueName);
+
+        stats.Name.ShouldBe(queueName);
         stats.MessageCount.ShouldBe((uint)0);
     }
 
