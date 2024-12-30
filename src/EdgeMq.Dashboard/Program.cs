78e27445fd7b58f3fce936b3234e4f94a684cf79
Martin Stanek <stanek@awitec.cz>
2024-12-30 19:17:29 +0100

    test

--- src/EdgeMq.Dashboard/Program.cs
+++ src/EdgeMq.Dashboard/Program.cs
@@ -6,8 +6,8 @@ using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.AddEdgeMqClient();
 builder.Services.AddMudServices();
-builder.Services.AddEdgeMqClient();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
