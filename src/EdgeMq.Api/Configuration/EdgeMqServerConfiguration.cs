8157a15ce476563596f180f91954d8c94071d122
Martin Stanek <stanek@awitec.cz>
2025-01-01 15:31:00 +0100

    env conf

--- src/EdgeMq.Api/Configuration/EdgeMqServerConfiguration.cs
+++ src/EdgeMq.Api/Configuration/EdgeMqServerConfiguration.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EdgeMq.Api.Configuration;
 
@@ -7,29 +8,61 @@ public sealed record EdgeMqServerConfiguration
 {
     public required QueueStoreMode Mode { get; init; } = QueueStoreMode.InMemory;
 
-    public required string Path { get; init; } = string.Empty;
+    public required string Path { get; init; } = "./queues";
 
-    public required IReadOnlyCollection<string> Queues { get; init; } = [];
+    public required IReadOnlyCollection<string> Queues { get; init; } = [ "default" ];
 
-    public ulong MaxMessageCount { get; init; } = 100_000;
+    public ulong MaxMessageCount { get; init; } = 1000000;
 
     public ulong MaxMessageSizeBytes { get; init; } = 536870912;
 
+    public ulong MaxBufferMessageCount { get; init; } = 1000;
+
+    public ulong MaxBufferMessageSizeBytes { get; init; } = 1048576;
+
     public uint DefaultBatchSize { get; init; } = 100;
+
+    public static EdgeMqServerConfiguration Empty => new()
+    {
+        Mode = QueueStoreMode.InMemory,
+        Path = string.Empty,
+        Queues = []
+    };
 
     public static EdgeMqServerConfiguration ReadFromEnvironment()
     {
-        var path = Environment.GetEnvironmentVariable("EDGEMQ_PATH") ?? "./queues";
-        var queues = Environment.GetEnvironmentVariable("EDGEMQ_QUEUES") ?? "default";
+        var defaultConfig = Empty;
+        var path = Environment.GetEnvironmentVariable("EDGEMQ_PATH") ?? defaultConfig.Path;
+        var queues = Environment.GetEnvironmentVariable("EDGEMQ_QUEUES") ?? defaultConfig.Queues.First();
         var mode = Enum.TryParse<QueueStoreMode>(Environment.GetEnvironmentVariable("EDGEMQ_MODE"), out var envMode)
             ? envMode
             : QueueStoreMode.InMemory;
+        var maxCount = ulong.TryParse(Environment.GetEnvironmentVariable("EDGEMQ_MAXCOUNT"), out var envMaxCount)
+            ? envMaxCount
+            : defaultConfig.MaxMessageCount;
+        var maxSize = ulong.TryParse(Environment.GetEnvironmentVariable("EDGEMQ_MAXSIZEBYTES"), out var envMaxSize)
+            ? envMaxSize
+            : defaultConfig.MaxMessageSizeBytes;
+        var maxBufferCount = ulong.TryParse(Environment.GetEnvironmentVariable("EDGEMQ_MAXBUFFERCOUNT"), out var envMaxBufferCount)
+            ? envMaxBufferCount
+            : defaultConfig.MaxBufferMessageCount;
+        var maxBufferSize = ulong.TryParse(Environment.GetEnvironmentVariable("EDGEMQ_MAXBUFFERSIZEBYTES"), out var envMaxBufferSize)
+            ? envMaxBufferSize
+            : defaultConfig.MaxBufferMessageSizeBytes;
+        var batchSize = uint.TryParse(Environment.GetEnvironmentVariable("EDGEMQ_BATCHSIZE"), out var envBatchSize)
+            ? envBatchSize
+            : defaultConfig.DefaultBatchSize;
 
         return new EdgeMqServerConfiguration
         {
             Mode = mode,
             Path = path,
-            Queues = queues.Split(',')
+            Queues = queues.Split(','),
+            MaxMessageCount = maxCount,
+            MaxMessageSizeBytes = maxSize,
+            MaxBufferMessageCount = maxBufferCount,
+            MaxBufferMessageSizeBytes = maxBufferSize,
+            DefaultBatchSize = batchSize
         };
     }
 }
