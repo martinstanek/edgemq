872891d7e1ea04893b60fbfdc29720d7e5742a31
Martin Stanek <stanekmartin@bluewin.ch>
2025-02-09 16:05:10 +0100

    openapi

--- src/EdgeMq.Api/Extensions/WebApplicationExtensions.cs
+++ src/EdgeMq.Api/Extensions/WebApplicationExtensions.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Immutable;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using EdgeMq.Api.Handlers;
+using EdgeMq.Model;
 
 namespace EdgeMq.Api.Extensions;
 
@@ -15,43 +17,49 @@ public static class WebApplicationExtensions
         var api = webApplication.MapGroup("/v1/queues");
 
         api.MapGet("/", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromServices] IEdgeQueueHandler handler)
-            => await handler.GetQueuesAsync(apiKey ?? string.Empty));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromServices] IEdgeQueueHandler handler)
+                => await handler.GetQueuesAsync(apiKey ?? string.Empty))
+            .Produces<ImmutableArray<Queue>>();
 
         api.MapGet("/{name}", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromRoute] string name,
-                [FromQuery] int batchSize,
-                [FromServices] IEdgeQueueHandler handler)
-            => await handler.DequeueAsync(name, apiKey ?? string.Empty, batchSize));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromRoute] string name,
+                    [FromQuery] int batchSize,
+                    [FromServices] IEdgeQueueHandler handler)
+                => await handler.DequeueAsync(name, apiKey ?? string.Empty, batchSize))
+            .Produces<ImmutableArray<QueueRawMessage>>();
 
         api.MapGet("/{name}/stats", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromRoute] string name,
-                [FromServices] IEdgeQueueHandler handler)
-            => await handler.GetMetricsAsync(name, apiKey ?? string.Empty));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromRoute] string name,
+                    [FromServices] IEdgeQueueHandler handler)
+                => await handler.GetMetricsAsync(name, apiKey ?? string.Empty))
+            .Produces<QueueMetrics>();
 
         api.MapGet("/{name}/peek", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromRoute] string name,
-                [FromQuery] int batchSize,
-                [FromServices] IEdgeQueueHandler handler)
-            => await handler.PeekAsync(name, apiKey ?? string.Empty, batchSize));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromRoute] string name,
+                    [FromQuery] int batchSize,
+                    [FromServices] IEdgeQueueHandler handler)
+                => await handler.PeekAsync(name, apiKey ?? string.Empty, batchSize))
+            .Produces<ImmutableArray<QueueRawMessage>>();
 
         api.MapPatch("/{name}", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromRoute] string name,
-                [FromQuery] Guid batchId,
-                [FromServices] IEdgeQueueHandler handler)
-            => await handler.AcknowledgeAsync(name, apiKey ?? string.Empty, batchId));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromRoute] string name,
+                    [FromQuery] Guid batchId,
+                    [FromServices] IEdgeQueueHandler handler)
+                => await handler.AcknowledgeAsync(name, apiKey ?? string.Empty, batchId))
+            .Produces(StatusCodes.Status204NoContent);
 
         api.MapPost("/{name}", async (
-                [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
-                [FromRoute] string name,
-                [FromServices] IEdgeQueueHandler handler,
-                HttpRequest request)
-            => await handler.EnqueueAsync(request, name, apiKey ?? string.Empty));
+                    [FromHeader(Name = EdgeApiKeyHeader)] string? apiKey,
+                    [FromRoute] string name,
+                    [FromServices] IEdgeQueueHandler handler,
+                    HttpRequest request)
+                => await handler.EnqueueAsync(request, name, apiKey ?? string.Empty))
+            .Produces<QueueEnqueueResult>();
 
         return webApplication;
     }
