84028f6e00fdd5ef473034d20a2d1c7769a6836c
Martin Stanek <stanek@awitec.cz>
2025-02-11 19:45:49 +0100

    ui & tests

--- tests/EdgeMq.Consumer/Program.cs
+++ tests/EdgeMq.Consumer/Program.cs
@@ -10,19 +10,21 @@ public static class Program
     {
         Console.WriteLine("EdgeMQ Consumer");
 
-        var r = new Random();
-        var httpClient = new HttpClient { BaseAddress = new Uri("http://10.0.1.106:2323") };
-        var edgeMqClient = new EdgeMqClient(httpClient);
+        var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:2323") };
+        var config = new EdgeMqClientConfiguration { ApiKey = "123" };
+        var edgeMqClient = new EdgeMqClient(httpClient, config);
         var source = new CancellationTokenSource();
         var token = source.Token;
+        var random = new Random();
 
         source.CancelAfter(TimeSpan.FromMinutes(10));
 
         while (!token.IsCancellationRequested)
         {
-            var batch = r.Next(1, 20);
+            var batch1 = random.Next(1, 200);
+            var batch2 = random.Next(1, 200);
 
-            await edgeMqClient.DequeueAsync("test-queue", batch, TimeSpan.FromSeconds(1), messages =>
+            await edgeMqClient.DequeueAsync("queue1", batch1, TimeSpan.FromSeconds(1), messages =>
             {
                 foreach (var message in messages)
                 {
@@ -33,7 +35,20 @@ public static class Program
 
             }, CancellationToken.None);
 
-            await Task.Delay(100, token);
+            await Task.Delay(TimeSpan.FromSeconds(random.Next(1, 5)), token);
+
+            await edgeMqClient.DequeueAsync("queue2", batch2, TimeSpan.FromSeconds(1), messages =>
+            {
+                foreach (var message in messages)
+                {
+                    Console.WriteLine($"{++_count} - {message.Payload}");
+                }
+
+                return Task.CompletedTask;
+
+            }, CancellationToken.None);
+
+            await Task.Delay(TimeSpan.FromSeconds(random.Next(1, 5)), token);
         }
     }
 }
--- tests/EdgeMq.Producer/Program.cs
+++ tests/EdgeMq.Producer/Program.cs
@@ -14,10 +14,12 @@ public static class Program
     {
         Console.WriteLine("EdgeMQ Producer");
 
-        var httpClient = new HttpClient { BaseAddress = new Uri("http://10.0.1.106:2323") };
-        var edgeMqClient = new EdgeMqClient(httpClient);
+        var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:2323") };
+        var config = new EdgeMqClientConfiguration { ApiKey = "123" };
+        var edgeMqClient = new EdgeMqClient(httpClient, config);
         var source = new CancellationTokenSource();
         var token = source.Token;
+        var random = new Random();
 
         source.CancelAfter(TimeSpan.FromMinutes(10));
 
@@ -25,11 +27,15 @@ public static class Program
         {
             var payload = DateTime.Now.ToString("s");
 
-            await edgeMqClient.EnqueueAsync("test-queue", payload);
+            await edgeMqClient.EnqueueAsync("queue1", payload);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(random.Next(1, 100)), token);
+
+            await edgeMqClient.EnqueueAsync("queue2", payload);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(random.Next(1, 100)), token);
 
             Console.WriteLine($"{++_count} - {payload}");
-
-            await Task.Delay(100, token);
         }
     }
 }
