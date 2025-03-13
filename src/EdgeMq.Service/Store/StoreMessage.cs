9aef508342211a319e7170630b5320d64aa76769
martinstanek <stanekmartin@bluewin.ch>
2025-03-13 08:53:47 +0100

    Develop (#7)
    * md
    
    * build
    
    * bump

--- src/EdgeMq.Service/Store/StoreMessage.cs
+++ src/EdgeMq.Service/Store/StoreMessage.cs
@@ -3,10 +3,8 @@ using System.Collections.ObjectModel;
 
 namespace EdgeMq.Service.Store;
 
-public struct StoreMessage
+public sealed record StoreMessage
 {
-    public StoreMessage() { }
-
     public string Payload { get; init; } = string.Empty;
 
     public IReadOnlyDictionary<string, string> Headers { get; init; } =  ReadOnlyDictionary<string, string>.Empty;
